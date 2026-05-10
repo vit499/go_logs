c815ebfd327c8ef0756c43e13ed4f83e140a5a72
aa <aa@m.ru>
2026-05-10 03:04:56 +0200

    x

--- pkg/utils/delays.go
+++ pkg/utils/delays.go
@@ -16,15 +16,16 @@ func D_1s(sec int) {
 }
 
 func GetDayTime() string {
-	// dv_time=20140101120029
-	t := time.Now()
+	loc := time.FixedZone("EET", 2*60*60)
+	t := time.Now().In(loc)
 	s1 := fmt.Sprintf("%04d%02d%02d%02d%02d%02d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
 	return s1
 }
 
+// \r\nhh:mm:ss_
 func GetTime() []byte {
-	// dv_time=20140101120029
-	t := time.Now()
+	loc := time.FixedZone("EET", 2*60*60)
+	t := time.Now().In(loc)
 	s1 := fmt.Sprintf("\r\n%02d:%02d:%02d ", t.Hour(), t.Minute(), t.Second())
 	b := []byte(s1)
 	return b
